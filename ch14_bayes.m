8cb6b58cc0512f75f31696aaf4cadba7e6ed85ce
Sid Zhang <zsg@outlook.com>
2022-07-19 19:37:47 +0800

    fix ch14-bayes.m bug

--- ch14_bayes.m
+++ ch14_bayes.m
@@ -1,7 +1,7 @@
 clc; clear; close all;
 
-fileNames = {'washDC_band1_blue.tif', 'washDC_band2_green.tif', ...
-    'washDC_band3_red.tif', 'washDC_band4_nrinfrared.tif'};
+fileNames = {'washDC-band1-blue.tif', 'washDC-band2-green.tif', ...
+    'washDC-band3-red.tif', 'washDC-band4-nrinfrared.tif'};
 for k = 1: length(fileNames)
     f{k} = im2double(imread(fileNames{k}));
     subplot(3,3,k), imshow(f{k});
@@ -14,11 +14,7 @@ Nc = 3;
 maskNames = {'washDC-mask-water.tif', 'washDC-mask-urban.tif', ...
     'washDC-mask-veg.tif'};
 for k = 1:length(maskNames)
-    bgmask = zeros(M, N);
-    fi = im2double(imread(maskNames{k}));
-    [mr, mc] = size(fi);
-    bgmask((M-mr)/2:(M+mr)/2 -1, (N-mc)/2:(N+mc)/2 -1) = fi;
-    mask{k} = bgmask;
+    mask{k} = im2double(imread(maskNames{k}));
 end
 maskimage = mask{1} | mask{2} | mask{3};
 subplot(3,3,5), imshow(maskimage);
@@ -35,23 +31,30 @@ for k = 1:Nc
 end
 
 Ctrain = cat(3, ctrain{1}, ctrain{2}, ctrain{3});
-Mtrain = cat(3, mtrain{1}, mtrain{2}, mtrain{3});
-Mtrain = reshape(Mtrain, Nc, numel(mtrain{1}));
+Mtrain = cat(2, mtrain{1}, mtrain{2}, mtrain{3})';
+
 
 for k = 1:Nc
     d{k} = bayesgauss(trainX{k}, Ctrain, Mtrain);
+    dtest{k} = bayesgauss(testX{k}, Ctrain, Mtrain);
 end
 
 for I = 1:Nc
     for J = 1:Nc
         classTrainSet(I, J) = numel(find(d{I} == J));
+        classTestSet(I, J) = numel(find(dtest{I} == J));        
     end
 end
 
-disp(classTrainSet)
-
 for k = 1:Nc
     rate(k) = classTrainSet(k, k)/sum(classTrainSet(k,:)) * 100;
+    testrate(k) = classTestSet(k, k)/sum(classTestSet(k,:)) * 100;
 end
 
+disp('training data:')
 disp(rate)
+disp(classTrainSet)
+
+disp('test data:')
+disp(classTestSet)
+disp(testrate)
